04f6289cb27cd51a82e70eac54f49429bbc0532c
Emil <emil.bernstorf@gmail.com>
2023-09-11 11:53:01 +0200

    Added Collectible

--- NameMakesNoSense/Assets/Scripts/PlayerMovementTestScript.cs
+++ NameMakesNoSense/Assets/Scripts/PlayerMovementTestScript.cs
@@ -5,20 +5,30 @@ using UnityEngine;
 public class PlayerMovementTestScript : MonoBehaviour
 {
     [SerializeField] private float speed = 2f;
+    [SerializeField] private float jumpForce = 50f;
+    [SerializeField] private Rigidbody playerBody;
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 movement = transform.eulerAngles;
+
         if(Input.GetAxis("Horizontal") != 0)
         {
-            Vector3 direction = transform.right * (Input.GetAxis("Horizontal"));
+            Vector3 direction = Vector3.right * (Input.GetAxis("Horizontal"));
             transform.Translate(direction * speed * Time.deltaTime);
         }
 
         if (Input.GetAxis("Vertical") != 0)
         {
-            Vector3 direction = transform.forward * (Input.GetAxis("Vertical")); //Vector3.up skulle göra att den flyger
+            Vector3 direction = Vector3.forward * (Input.GetAxis("Vertical")); //Vector3.up skulle göra att den flyger
             transform.Translate(direction * speed * Time.deltaTime);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerBody.AddForce(Vector3.up * jumpForce);
         }
     }
 }
