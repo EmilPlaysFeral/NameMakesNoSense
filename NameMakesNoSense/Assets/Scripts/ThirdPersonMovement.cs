b26676be09ac64a954483997c23be8e2246de7c0
Emil <emil.bernstorf@gmail.com>
2023-09-19 14:44:37 +0200

    MushroomMadnessScene

--- NameMakesNoSense/Assets/Scripts/ThirdPersonMovement.cs
+++ NameMakesNoSense/Assets/Scripts/ThirdPersonMovement.cs
@@ -8,9 +8,19 @@ public class ThirdPersonMovement : MonoBehaviour
     public Transform cam;
 
     public float speed = 6f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
 
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
+
+    Vector3 velocity;
+    bool isGrounded;
+    
 
     private void Start()
     {
@@ -20,6 +30,13 @@ public class ThirdPersonMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f; 
+        }
+
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
@@ -32,6 +49,15 @@ public class ThirdPersonMovement : MonoBehaviour
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
     }
 }
